3897c0f24e9928caf2927107a026c5230f464796
«Seregin_by» <Seregin_by@mail.ru>
2022-02-07 00:55:28 +0300

    выполнены

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,2 +1,22 @@
 ///<reference types="Cypress" />
 
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Вхід');
+})  
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Вхід')
+})  
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Вхід', {matchCase: false});
+})  
+
+it.only('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.wait(6000)
+    cy.get('div').contains('Read more').click();
+})  
