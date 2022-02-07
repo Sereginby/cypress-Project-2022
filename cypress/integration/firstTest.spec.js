b105ff9b33beb699db32dab8065ab6d30fa3621f
«Seregin_by» <Seregin_by@mail.ru>
2022-02-07 16:44:21 +0300

    9 tests S+D

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,22 +1,61 @@
 ///<reference types="Cypress" />
 
-it('', () => {
+
+it('Should', () => {
     cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.contains('Вхід');
+    cy.get('[data-qa-node="amount"]')
+    .type(100)
+    .should('have.value',100)
+    .and('be.visible')
 })  
 
-it('', () => {
+it('Expect', () => {
     cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.contains('div', 'Вхід')
+    cy.get('[data-qa-node="amount"]')
+      .type(100).then( input=> {
+       expect(input).to.have.value(100)
+    })  
 })  
 
-it('', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.contains('Вхід', {matchCase: false});
-})  
+               
+ it('check default value for deposits', ()=> {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.get('[data-qa-value="UAH"]')
+        .should('be.checked')   
+ })                 
 
-it.only('', () => {
+ it('check is visible Archive link', ()=> {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.contains('Мої депозити')
+       .trigger('mouseover') 
+       .get('#archiveDeposits')
+       .should('be.visible')     
+ }) 
+
+ it('check is correct attr in button', ()=> {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.contains('Show cards')
+        .should('have.attr','type')  
+        .and('match', /button/) 
+ })      
+
+ 
+ it('contains Diana',() => {
     cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.wait(6000)
-    cy.get('div').contains('Read more').click();
-})  
+    cy.contains('Sign in')
+})
+
+it('contains Diana',() => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div','Sign in')
+})
+
+it('contains Diana',() => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN IN',{matchCase:false})
+})
+
+it.only('contains Diana',() => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('').contains('')
+})
