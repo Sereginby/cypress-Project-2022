b0fcee37b7ef160899e0f6f6e7ce33a131bbf98e
«Seregin_by» <Seregin_by@mail.ru>
2022-02-21 17:24:58 +0300

    17.25  21.02

--- cypress/integration/2-advanced-examples/Test upload_File.js
+++ cypress/integration/2-advanced-examples/Test upload_File.js
@@ -1,10 +1,16 @@
 
 
 //Example HTTP the Get request
-it('Example  Get request', () => {
+it.skip('Example  Get request', () => {
     cy.request('https://social-network.samuraijs.com/api/1.0/users')
+        .then((responce) => {
+            console.log(responce.body.items);
+        })
+})
+ 
+it('Example  Get request 2', () => {
+    cy.request('https://queuev4.vk.com/im1576')
         .then((responce) => {
             console.log(responce);
         })
 })
- 
